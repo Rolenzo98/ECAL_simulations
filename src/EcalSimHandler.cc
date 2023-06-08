1fdc4c802f2a15217424792ac675406a099f3f5e
Rolenzo98 <lorenzoboccia98@gmail.com>
2023-06-08 14:34:51 +0200

    Added the hist1d for time with different otpions for the bin size etc

--- src/EcalSimHandler.cc
+++ src/EcalSimHandler.cc
@@ -23,9 +23,11 @@ void EcalSimHandler::Loop()
 
   // Event Loop
   int tothits = 0;
+  float binsize_h1htime=0.001;
 
-  TH3D *h3h;
-  TH3D *h3h1;
+  TH3D *h3hpos;
+  TH3D *h3hpos1;
+  TH1F *h1htime;
 
   // TH1D 
   TFile *storemyfile = new TFile("myfileh3h.root", "recreate");
@@ -65,26 +67,33 @@ void EcalSimHandler::Loop()
       
       if (jentry==10)
       {
-        //   h3h=new TH3D(TString::Format("h3h_%lld",jentry),"Hist in 3D",
+        //   h3hpos=new TH3D(TString::Format("h3hpos_%lld",jentry),"Hist in 3D",
         //   5,TMath::MinElement(nsch, scpox),TMath::MaxElement(nsch, scpox),
         //   5,TMath::MinElement(nsch, scpoy),TMath::MaxElement(nsch, scpoy),
         //   5,TMath::MinElement(nsch, scpoz),TMath::MaxElement(nsch, scpoz));
 
-        //   h3h1 = new TH3D(TString::Format("h3h1_%lld",jentry),"Hist in 3D",
+        //   h3hpos1 = new TH3D(TString::Format("h3hpos1_%lld",jentry),"Hist in 3D",
         //   100,TMath::MinElement(nsch, scpox),TMath::MaxElement(nsch, scpox),
         //   100,TMath::MinElement(nsch, scpoy),TMath::MaxElement(nsch, scpoy),
         //   100,TMath::MinElement(nsch, scpoz),TMath::MaxElement(nsch, scpoz));
+        // int nbins=std::ceil((TMath::MaxElement(nsch,sctime)-TMath::MinElement(nsch,sctime))/binsize_h1htime);
+        h1htime = new TH1F(TString::Format("h1htime_%lld",jentry), "Hist 1D time of hits of event ", 1000/0.01, 0, 100);
+        // h1htime = new TH1F(TString::Format("h1htime_%lld",jentry), "Hist 1D time of hits of event ", nbins, 0, TMath::MaxElement(nsch,sctime));
+        // h1htime = new TH1F(TString::Format("h1htime_%lld",jentry), "Hist 1D time of hits of event "+std::to_string(jentry), nbins, 0, TMath::MaxElement(nsch,sctime));
+        // h1htime->SetTitle("Hist 1D time of hits of event "+std::to_string(jentry)+";time (ps);counts");
+        h1htime->SetTitle("Hist 1D time of hits of event ;time (ps);counts");
 
-        // hists[jentry]=new TH3D(TString::Format("h3h_%d",jentry),"Hist in 3D",100,TMath::MinElement(nsch, scpox),TMath::MaxElement(nsch, scpox),200,TMath::MinElement(nsch, scpoy),TMath::MaxElement(nsch, scpoy),300,TMath::MinElement(nsch, scpoz),TMath::MaxElement(nsch, scpoz));
-        // h3h->SetTitle("Hist in 3D;X;Y;Z");
-        // h3h1->SetTitle("Hist in 3D;X;Y;Z");
+        // hists[jentry]=new TH3D(TString::Format("h3hpos_%d",jentry),"Hist in 3D",100,TMath::MinElement(nsch, scpox),TMath::MaxElement(nsch, scpox),200,TMath::MinElement(nsch, scpoy),TMath::MaxElement(nsch, scpoy),300,TMath::MinElement(nsch, scpoz),TMath::MaxElement(nsch, scpoz));
+        // h3hpos->SetTitle("Hist in 3D;X;Y;Z");
+        // h3hpos1->SetTitle("Hist in 3D;X;Y;Z");
 
         for (int ihit=0; ihit<nsch;ihit++)
         {
           cout<<"hit "<<ihit<<" at time "<<sctime[ihit]<<endl;
-          // h3h->Fill((Double_t)scpox[ihit],(Double_t)scpoy[ihit],(Double_t)scpoz[ihit]);
-          // h3h->Fill(scpox[ihit],scpoy[ihit],scpoz[ihit]);
-          // h3h1->Fill(scpox[ihit],scpoy[ihit],scpoz[ihit]);
+          // h3hpos->Fill((Double_t)scpox[ihit],(Double_t)scpoy[ihit],(Double_t)scpoz[ihit]);
+          // h3hpos->Fill(scpox[ihit],scpoy[ihit],scpoz[ihit]);
+          // h3hpos1->Fill(scpox[ihit],scpoy[ihit],scpoz[ihit]);
+          h1htime->Fill(sctime[ihit]);
           // cout << "hit: " << ihit << "\t x: " << scpox[ihit] << "\t y: " << scpoy[ihit] << "\t z: " << scpoz[ihit] << endl;
         }
       }
@@ -93,8 +102,9 @@ void EcalSimHandler::Loop()
 
   // MODIFY FROM HERE
   cout<<"\tTotal hits: "<<tothits<<endl;
-  // h3h->Write();
-  // h3h1->Write();
+  // h3hpos->Write();
+  // h3hpos1->Write();
+  h1htime->Write();
   storemyfile->Close();
 
   // TIL HERE
