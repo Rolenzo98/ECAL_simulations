4414f23af6e0603482375fce1e0a4decc98b85d7
Rolenzo98 <lorenzo.boccia@universite-paris-saclay.fr>
2023-06-09 19:21:19 +0200

    modified variables name for the bins of time. Added graphs in 2D and 3D and arrrays of them. Added breaks to faster (maybe) the code. Saved canvas and histograms in the file myfilerooth3h. Added time measurement of the program run.

--- src/EcalSimHandler.cc
+++ src/EcalSimHandler.cc
@@ -25,30 +25,43 @@ void EcalSimHandler::Loop()
   int tothits = 0;
   float binsize_h1htime=0.001;
 
-  int nbinsarr=24;
-  double expvalarr[nbinsarr+1];
-  // vector<TH1F*> h2times;
-  TH2F *h2times[nbinsarr];
+  int nbin=24;
+  int nbinlowedge=nbin+1;
+  double binlowedge[nbinlowedge];
 
-  TH3D *h3hpos;
-  TH3D *h3hpos1;
   TH1F *h1htime;
 
+  TH2F *h2htime[nbin];
+  TH3D *h3hpostime[nbin];
 
-  for (int i=0;i<=nbinsarr;i++)
+  TH3D *h3hpos;
+  TH3D *h3hpostimeTOT;
+
+  for (int i=0;i<nbinlowedge;i++)
   {
     int ipow=i-10;
-    expvalarr[i]=std::pow(2,ipow);
-    cout<<expvalarr[i]<<" ";
-    TString histname=TString::Format("h2times_%d",expvalarr[i]);
-    h2times[i]=new TH2F(histname,histname,100,-500,500,100,-0,3000);
-    // h2times.push_back(new TH1F(histname,histname,));
+    binlowedge[i]=std::pow(2,ipow);
+    cout<<"Bin "<<i<<" starts at time "<<binlowedge[i]<<endl;
+
+  }
+
+  for (int i=0;i<nbin;i++)
+  {
+    TString h2htime_name=TString::Format("h2htime_bin%d",i);
+    TString h3hpostime_name=TString::Format("h3hpostime_bin%d",i);
+
+    h2htime[i]=new TH2F(h2htime_name,h2htime_name,100,-500,500,100,-0,3000);
+
+    
+    h3hpostime[i]= new TH3D(h3hpostime_name,h3hpostime_name,
+    100,-2000,2000,
+    100,-1000,5000,
+    100,-3000,3000);
   }
 
 
-  TFile *storemyfile = new TFile("myfileh3h.root", "recreate");
-
-  for (Long64_t jentry = 0; jentry < nentries; jentry++)
+  
+  for (Long64_t jentry =0; jentry<nentries; jentry++)
   {
     Long64_t ientry = LoadTree(jentry);
     if (ientry < 0)
@@ -60,7 +73,7 @@ void EcalSimHandler::Loop()
       continue;
 
     // MODIFY FROM HERe
-      // cout << "Event : " << jentry << "\tHits: " << nsch << "\tSize array: " << sizeof(scpox) / sizeof(Float_t) << endl;
+      cout << "Event : " << jentry << "\tHits: " << nsch << "\tSize array: " << sizeof(scpox) / sizeof(Float_t) << endl;
       tothits+=nsch;
 
       /*
@@ -82,17 +95,18 @@ void EcalSimHandler::Loop()
       
       if (jentry==10)
       {
-        // h3hpos=new TH3D(TString::Format("h3hpos_%lld",jentry),"Hist in 3D",
-        // 5,TMath::MinElement(nsch, scpox),TMath::MaxElement(nsch, scpox),
-        // 5,TMath::MinElement(nsch, scpoy),TMath::MaxElement(nsch, scpoy),
-        // 5,TMath::MinElement(nsch, scpoz),TMath::MaxElement(nsch, scpoz));
-        // h3hpos->SetTitle("Hist in 3D;X;Y;Z");
 
-        // h3hpos1 = new TH3D(TString::Format("h3hpos1_%lld",jentry),"Hist in 3D",
+        // h3hpos= new TH3D(TString::Format("h3hpos_%lld",jentry),"Hist in 3D",
         // 100,TMath::MinElement(nsch, scpox),TMath::MaxElement(nsch, scpox),
         // 100,TMath::MinElement(nsch, scpoy),TMath::MaxElement(nsch, scpoy),
         // 100,TMath::MinElement(nsch, scpoz),TMath::MaxElement(nsch, scpoz));
-        // h3hpos1->SetTitle("Hist in 3D;X;Y;Z");
+        // h3hpos->SetTitle("Hist in 3D;X;Y;Z");        
+        
+        // h3hpostimeTOT= new TH3D(TString::Format("h3hpostimeTOT_%lld",jentry),"Hist in 3D",
+        // 100,TMath::MinElement(nsch, scpox),TMath::MaxElement(nsch, scpox),
+        // 100,TMath::MinElement(nsch, scpoy),TMath::MaxElement(nsch, scpoy),
+        // 100,TMath::MinElement(nsch, scpoz),TMath::MaxElement(nsch, scpoz));
+        // h3hpostimeTOT->SetTitle("Hist in 3D;X;Y;Z");
         
 
         // int nbins=std::ceil((TMath::MaxElement(nsch,sctime)-TMath::MinElement(nsch,sctime))/binsize_h1htime);
@@ -105,40 +119,85 @@ void EcalSimHandler::Loop()
         for (int ihit=0; ihit<nsch;ihit++)
         {
           // cout<<"hit "<<ihit<<" at time "<<sctime[ihit]<<endl;
-
-          // h3hpos->Fill((Double_t)scpox[ihit],(Double_t)scpoy[ihit],(Double_t)scpoz[ihit]);
           // h3hpos->Fill(scpox[ihit],scpoy[ihit],scpoz[ihit]);
-          // h3hpos1->Fill(scpox[ihit],scpoy[ihit],scpoz[ihit]);
           // h1htime->Fill(sctime[ihit]);
 
-          for (int indexh2t=0;indexh2t<nbinsarr;indexh2t++)
+          for (int i=0;i<nbin;i++)
           {
-            if (sctime[ihit]>expvalarr[indexh2t]&&sctime[ihit]<expvalarr[indexh2t+1])
+            if (sctime[ihit]>binlowedge[i]&&sctime[ihit]<binlowedge[i+1])
             {
-              h2times[indexh2t]->Fill(scpox[ihit],scpoy[ihit]);
+              h2htime[i]->Fill(scpox[ihit],scpoy[ihit]);
+              h3hpostime[i]->Fill(scpox[ihit],scpoy[ihit],scpoz[ihit]);
+              break;
             }
           }
 
           // cout << "hit: " << ihit << "\t x: " << scpox[ihit] << "\t y: " << scpoy[ihit] << "\t z: " << scpoz[ihit] << endl;
         }
+        // break;
       }
     // TIL HERE
   }
 
-
-
-
   // MODIFY FROM HERE
   cout<<"\tTotal hits: "<<tothits<<endl;
+  TFile *storemyfile = new TFile("myfileh3h.root", "recreate");
 
-  for (int indexh2t=0;indexh2t<nbinsarr;indexh2t++)
+
+  TCanvas *c1=new TCanvas("c1","c1",500,500);
+  // h3hpostime[16]->SetMarkerStyle(20);
+  // h3hpostime[11]->SetMarkerStyle(20);
+  // h3hpostime[12]->SetMarkerStyle(20);
+  // h3hpostime[16]->SetMarkerSize(2);
+  // h3hpostime[11]->SetMarkerSize(2);
+  // h3hpostime[12]->SetMarkerSize(2);
+  //     h3hpostime[16]->SetMarkerColor(kRed);
+  //   h3hpostime[11]->SetMarkerColor(kBlue);
+  //   h3hpostime[12]->SetMarkerColor(kGreen);
+
+    // h3hpostime[13]->SetMarkerColor();
+    // h3hpostime[14]->SetMarkerColor(5);
+    // h3hpostime[15]->SetMarkerColor(6);
+
+    // h3hpostime[16]->Draw(" SAME");
+    // h3hpostime[11]->Draw(" SAME");
+    // h3hpostime[12]->Draw(" SAME");
+    // h3hpostime[13]->Draw("BOX SAME");
+    // h3hpostime[14]->Draw("BOX SAME");
+    // h3hpostime[15]->Draw("BOX  SAME");
+  for (int i=0;i<nbin;i++)
   {
-    h2times[indexh2t]->Write();
+    h3hpostime[i]->SetMarkerStyle(20);
+    h3hpostime[11]->SetMarkerSize(0.2);
+    h3hpostime[i]->SetMarkerColor(i);;
+    h3hpostime[i]->Draw("SAME");
   }
+  c1->Write();
+
+
+  for (int i=0;i<nbin;i++)
+  {
+    h2htime[i]->SetTitle(TString::Format("h2htime_bin%d_(%f-%f)",i,binlowedge[i],binlowedge[i+1]));
+    h2htime[i]->Write();
+
+  }
+  
+    //   h3hpostime[16]->SetMarkerColor(kRed);
+    // h3hpostime[11]->SetMarkerColor(kBlue);
+    // h3hpostime[12]->SetMarkerColor(kGreen);
+  for (int i=0;i<nbin;i++)
+  {
+    h3hpostime[i]->SetTitle(TString::Format("h3hpostime_bin%d_(%f-%f)",i,binlowedge[i],binlowedge[i+1]));
+    h3hpostime[i]->Write();
+  }
+
   // h3hpos->Write();
-  // h3hpos1->Write();
   // h1htime->Write();
   storemyfile->Close();
 
   // TIL HERE
+  auto stop = high_resolution_clock::now();
+  auto duration = duration_cast<microseconds>(stop - start);
+  cout << "Time taken by program: "<< duration.count() << " microseconds" << endl;
+
 }
