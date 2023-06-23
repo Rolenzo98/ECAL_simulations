5e2317a45c7b5123dbb740bca3249afce3bbfe31
Rolenzo98 <lorenzo.boccia@universite-paris-saclay.fr>
2023-06-23 18:12:10 +0200

    Solved the colors of the points in the 3D

--- src/EcalSimHandler.cc
+++ src/EcalSimHandler.cc
@@ -131,6 +131,7 @@ void EcalSimHandler::Loop()
       std::filesystem::create_directory("gif/2xz");
       std::filesystem::create_directory("gif/3");
 
+  cout<<"Event\tHits\ttime(s)\ttot(s)\tremaining"<<endl;
   // MAIN` LOOP - Loop over the events (100 times)
   for (Long64_t jentry =0; jentry<nentries; jentry++)
   {
@@ -147,81 +148,20 @@ void EcalSimHandler::Loop()
     // energyevent=0;
     tothits+=nsch;
 
-    TString::Format("h1htime_%d",jentry);
     TCanvas *c_2timexy_gif=new TCanvas(TString::Format("c%lld_2timexy_gif",jentry),TString::Format("c%lld_2timexy_gif",jentry),500,500);
     TCanvas *c_2timeyz_gif=new TCanvas(TString::Format("c%lld_2timeyz_gif",jentry),TString::Format("c%lld_2timeyz_gif",jentry),500,500);
     TCanvas *c_2timexz_gif=new TCanvas(TString::Format("c%lld_2timexz_gif",jentry),TString::Format("c%lld_2timexz_gif",jentry),500,500);
-    TCanvas *c_3time_gif=new TCanvas(TString::Format("c%lld_3time_gif",jentry),TString::Format("c%lld_3time_gif",jentry),500,500);
-    TCanvas *c_3energy_gif=new TCanvas(TString::Format("c%lld_3energy_gif",jentry),TString::Format("c%lld_3energy_gif",jentry),500,500);
-
-    // TCanvas *c_2timeyz_gif=new TCanvas("c_2timeyz_gif","c_2timeyz_gif",500,500);
-    // TCanvas *c_2timexz_gif=new TCanvas("c_2timexz_gif","c_2timexz_gif",500,500);
-    // TCanvas *c_3time_gif=new TCanvas("c_3time_gif","c_3time_gif",500,500); 
-
-    TCanvas *c_3time=new TCanvas(TString::Format("c%lld_3time",jentry),TString::Format("c%d_3time",jentry),500,500);
-
-    
-    TCanvas *c_3energy=new TCanvas(TString::Format("c%lld_3energy",jentry),TString::Format("c%d_3energy",jentry),500,500);
+    TCanvas *c_3time_gif=new TCanvas(TString::Format("c%lld_3time_gif",jentry),TString::Format("c%lld_3time_gif",jentry),1000,1000);
+    TCanvas *c_3time=new TCanvas(TString::Format("c%lld_3time",jentry),TString::Format("c%lld_3time",jentry),500,500);
+    TCanvas *c_3energy=new TCanvas(TString::Format("c%lld_3energy",jentry),TString::Format("c%lld_3energy",jentry),500,500);
 
 
-    
-    /*
-    // for (int i=0;i<nbin;i++)
-    // {
-    //   TString h2hxytime_name=TString::Format("h2hxytime_bin%d",i);
-    //   TString h2hyztime_name=TString::Format("h2hyztime_bin%d",i);
-    //   TString h2hxztime_name=TString::Format("h2hxztime_bin%d",i);
-    //   TString h3hpostime_name=TString::Format("h3hpostime_bin%d",i);
-    //   TString h3hposenergy_name=TString::Format("h3hposenergy_bin%d",i);
 
-    //   h2hxytime[i]=new TH2F(h2hxytime_name,h2hxytime_name,
-    //     100,h_xmin,h_xmax,100,h_ymin,h_ymax);
-    //   h2hyztime[i]=new TH2F(h2hyztime_name,h2hyztime_name,
-    //     100,h_ymin,h_ymax,100,h_zmin,h_zmax);
-    //   h2hxztime[i]=new TH2F(h2hxztime_name,h2hxztime_name,
-    //     100,h_xmin,h_xmax,100,h_zmin,h_zmax);
+      cout<<jentry<<"/"<<nentries<<"\t"<<nsch;
 
-    //   h3hpostime[i]= new TH3D(h3hpostime_name,h3hpostime_name,
-    //     100,h_xmin3D,h_xmax3D,
-    //     100,h_ymin3D,h_ymax3D,
-    //     100,h_zmin3D,h_zmax3D);
-    //   h3hposenergy[i]= new TH3D(h3hposenergy_name,h3hposenergy_name,
-    //     100,h_xmin3D,h_xmax3D,
-    //     100,h_ymin3D,h_ymax3D,
-    //     100,h_zmin3D,h_zmax3D);
-    // }
-    */
-    /*
-    // even if the number of hits is less than the size of the array, the remaining
-    // entries are filled with numbers, thus the minimum (or max) o the values of the 
-    // hits is not the same of the values of the array in its entireness
-
-    cout<<"min element in scpox-size "<<TMath::MinElement(sizeof(scpox) / sizeof(Float_t), scpox)<<endl;
-    cout<<"min element in scpox-hits "<<TMath::MinElement(nsch, scpox)<<endl;
-    if (TMath::MinElement(sizeof(scpox) / sizeof(Float_t), scpox)-TMath::MinElement(nsch, scpox)!=0)
+    if (jentry==58)
     {
-      cout<<"sizelocmin: "<<TMath::LocMin(sizeof(scpox) / sizeof(Float_t), scpox)<<" hitslocmin: "<<TMath::LocMin(nsch, scpox)<<endl;
-      for (int ihit=0; ihit<sizeof(scpox) / sizeof(Float_t);ihit++)
-      {
-        cout << "hit: " << ihit << "\t x: " << scpox[ihit] <<endl;
-      }      
-    }
-    */
-    /*
-    for (int ihit=0; ihit<nsch;ihit++)
-      {
-        // cout<<"energy of hit is "<<scene[ihit]<<endl;
-        energyevent+=scene[ihit];
-      }
-    */
-    
-    // Selecting a specific event (back to 1 time)
-    // EVENTUALLY THIS CONDITION WILL BE REMOVED
-      cout<<"Event: "<<jentry<<" Hits: "<<nsch;
 
-    // if (jentry==58)
-    // {
-      // cout<<"Event "<<jentry<<"analysis:\n"<<"Hit\tTime\t\tEnergy\tPosition"<<endl;
           
       h_xmin3D=TMath::MinElement(nsch, scpox);
       h_xmax3D=TMath::MaxElement(nsch, scpox);
@@ -236,19 +176,19 @@ void EcalSimHandler::Loop()
         100, h_xmin3D, h_xmax3D, 
         100, h_ymin3D, h_ymax3D, 
         100, h_zmin3D, h_zmax3D);
-      // h3hpos->SetDirectory(dir_h3);
+
       
       // Initializing the histogram for the time of the hits in 1D (valid all events, all single ones)
       h1htime=new TH1F(TString::Format("h1htime_%lld",jentry), 
       TString::Format("h1htime_%lld; time (ns); counts",jentry),
       nbin, tbinlowedge);
-      h1htime->SetDirectory(dir_h1);
+
 
       // Initializing the histogram for the energy of the hits in 1D (valid all events, all single ones)
       h1henergy=new TH1F(TString::Format("h1henergy_%lld",jentry),
       TString::Format("h1henergy_%lld; energy (keV); counts",jentry),
       nbin, ebinlowedge);
-      h1henergy->SetDirectory(dir_h1);
+
 
       for (int ibin=0; ibin<nbin; ibin++)
       {
@@ -265,29 +205,34 @@ void EcalSimHandler::Loop()
         TString h3hposenergy_name=TString::Format("Event_%lld_h3hposenergy_bin%d",jentry,ibin);
         TString h3hposenergy_title=TString::Format("Event_%lld_energy_bin%d_(%f-%f);X;Y;Z",jentry,ibin,ebinlowedge[ibin],ebinlowedge[ibin+1]);
 
+        // h3hpostime[ibin]= new TH3D(h3hpostime_name,h3hpostime_title,
+        //   100,h_xmin3D,h_xmax3D,
+        //   100,h_ymin3D,h_ymax3D,
+        //   100,h_zmin3D,h_zmax3D);          
         h3hpostime[ibin]= new TH3D(h3hpostime_name,h3hpostime_title,
-          100,h_xmin3D,h_xmax3D,
-          100,h_ymin3D,h_ymax3D,
-          100,h_zmin3D,h_zmax3D);  
-        h3hpostime[ibin]->SetDirectory(dir_h3);
+          100,h_xmin,h_xmax,
+          100,h_ymin,h_ymax,
+          100,h_zmin,h_zmax);  
+
 
         h3hposenergy[ibin]= new TH3D(h3hposenergy_name,h3hposenergy_title,
           100,h_xmin3D,h_xmax3D,
           100,h_ymin3D,h_ymax3D,
           100,h_zmin3D,h_zmax3D);
-        h3hposenergy[ibin]->SetDirectory(dir_h3);
+
+        
 
         h2hxytime[ibin]=new TH2F(h2hxytime_name,h2hxytime_title,
           100,h_xmin,h_xmax,100,h_ymin,h_ymax);
-        h2hxytime[ibin]->SetDirectory(dir_h2);
+
 
         h2hyztime[ibin]=new TH2F(h2hyztime_name,h2hyztime_title,
           100,h_ymin,h_ymax,100,h_zmin,h_zmax);
-        h2hyztime[ibin]->SetDirectory(dir_h2);
+
 
         h2hxztime[ibin]=new TH2F(h2hxztime_name,h2hxztime_title,
           100,h_xmin,h_xmax,100,h_zmin,h_zmax);
-        h2hxztime[ibin]->SetDirectory(dir_h2);
+
 
         for (int ihit=0; ihit<nsch;ihit++)
         {
@@ -309,21 +254,28 @@ void EcalSimHandler::Loop()
           }  
             
         }
+        gStyle->SetPalette(kRainBow);
+
 
         // gStyle->SetOptStat(0);
 
-        // dir_h2_xy->cd();
-        // h2hxytime[ibin]->Write();
-        // dir_h2_yz->cd();
-        // h2hyztime[ibin]->Write();
-        // dir_h2_xz->cd();
-        // h2hxztime[ibin]->Write();
+        dir_h2_xy->cd();
+        h2hxytime[ibin]->Write();
+        dir_h2_yz->cd();
+        h2hyztime[ibin]->Write();
+        dir_h2_xz->cd();
+        h2hxztime[ibin]->Write();
 
-        // dir_h3->cd();
-        // h3hpostime[ibin]->Write();
+        dir_h3->cd();
+        h3hpostime[ibin]->Write();
+        
+        canvases->cd();
         c_3time_gif->cd();
+        h3hpostime[ibin]->SetMarkerStyle(20);
+        h3hpostime[ibin]->SetMarkerSize(0.7);
+        h3hpostime[ibin]->SetMarkerColor(TColor::GetColorPalette(ibin*(254/nbin)));
         h3hpostime[ibin]->Draw();
-        c_3time_gif->Print(TString::Format("gif/3/c%lld_3time_gif.gif+20",jentry)); 
+        c_3time_gif->Print(TString::Format("gif/3/c%lld_3time_gif.gif+100",jentry)); 
 
 
         h2hxytime[ibin]->SetMarkerStyle(20);
@@ -336,15 +288,15 @@ void EcalSimHandler::Loop()
         h3hpostime[ibin]->SetMarkerStyle(20);
         h3hpostime[ibin]->SetMarkerSize(0.5);
         h3hposenergy[ibin]->SetMarkerStyle(20);
-        h3hposenergy[ibin]->SetMarkerSize(0.5);
-
-
-
-        h3hpostime[ibin]->SetMarkerColor(ibin);
+        h3hposenergy[ibin]->SetMarkerSize(0.7);
         h3hposenergy[ibin]->SetMarkerColor(ibin);
 
 
+
+
+
         c_2timexy_gif->cd();
+        h2hxytime[ibin]->SetMarkerColor(TColor::GetColorPalette(ibin*(254/nbin)));
         h2hxytime[ibin]->Draw();    
         myline1=new TLine(h_xmin, h_yECALmin, h_xmax, h_yECALmin);
         myline2=new TLine(h_xmin, h_yHCALmax, h_xmax, h_yHCALmax);
@@ -354,7 +306,7 @@ void EcalSimHandler::Loop()
         myline2->Draw("same");
         myline3->Draw("same");
         myline4->Draw("same");
-        c_2timexy_gif->Print(TString::Format("gif/2xy/c%lld_2timexy_gif.gif+20",jentry));  
+        c_2timexy_gif->Print(TString::Format("gif/2xy/c%lld_2timexy_gif.gif+50",jentry));  
         delete myline1;
         delete myline2;
         delete myline3;
@@ -362,6 +314,7 @@ void EcalSimHandler::Loop()
         delete h2hxytime[ibin];
 
         c_2timeyz_gif->cd();
+        h2hyztime[ibin]->SetMarkerColor(TColor::GetColorPalette(ibin*(254/nbin)));
         h2hyztime[ibin]->Draw();
         myline1=new TLine(h_yECALmin,h_zmin, h_yECALmin, h_zmax);
         myline2=new TLine(h_yHCALmax,h_zmin, h_yHCALmax, h_zmax);
@@ -379,6 +332,7 @@ void EcalSimHandler::Loop()
         delete h2hyztime[ibin];
 
         c_2timexz_gif->cd();
+        h2hxztime[ibin]->SetMarkerColor(TColor::GetColorPalette(ibin*(254/nbin)));
         h2hxztime[ibin]->Draw();
         c_2timexz_gif->Print(TString::Format("gif/2xz/c%lld_2timexz_gif.gif+20",jentry));  
         delete h2hxztime[ibin];
@@ -404,10 +358,9 @@ void EcalSimHandler::Loop()
       }
 
       dir_h1->cd();
-      h1htime->Write();
+      h1htime->Write("AXIG");
       h1henergy->Write();
       dir_h3->cd();
-      // h3hpos->SetDirectory(dir_h3);
       h3hpos->Write();
       
       delete h1htime;
@@ -415,7 +368,7 @@ void EcalSimHandler::Loop()
       delete h3hpos;
       
 
-    // } // end if for the specific event
+    } // end if for the specific event
     delete c_2timexy_gif;
     delete c_2timeyz_gif;
     delete c_2timexz_gif;
@@ -425,17 +378,14 @@ void EcalSimHandler::Loop()
     
 
     auto stopevent = high_resolution_clock::now(); 
-    auto durationevent = duration_cast <milliseconds>(stopevent - startevent);
-    auto duration = duration_cast <seconds>(stopevent - start);
-    // long int duration_int=static_cast<long int>(duration);
-    // long int jentry_int=static_cast<long int>(jentry);
-    // long int nentries_int=nentries;
-    // int cacca=jentry+1;
-    // auto pippo=duration.count()/(cacca)*(nentries-jentry);
-    
-    cout << "\tdone it: "<< durationevent.count() << " ms" << 
-    "\tTime total ("<<jentry<<"/"<<nentries<<"): "<< duration.count() << " s" << 
-    "\tEstimated remaining time: "<<(duration.count()/(jentry+1))*(nentries-jentry-1)<<" s"<<endl;
+    auto durationevent = duration_cast <seconds>(stopevent - startevent);
+    auto duration = duration_cast <seconds>(stopevent - start);    
+    cout <<
+    "\t"<< durationevent.count() << 
+    "\t"<< duration.count() <<  
+    "\t"<< ((duration.count()/(jentry+1))*(nentries-jentry-1))/60 << "' " <<
+    ((duration.count()/(jentry+1))*(nentries-jentry-1))%60 << "\"" << endl;;
+
 
 
 
@@ -445,7 +395,7 @@ void EcalSimHandler::Loop()
   storemyfile->Close();
   // delete storemyfile;
 
-  gSystem->Exec("root -l myfileh3h.root"); // execute the root file
+  gSystem->Exec("rootbrowse myfileh3h.root"); // execute the root file
   
   // time check
   auto stop= high_resolution_clock::now(); 
